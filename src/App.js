79cd3c27537eacb45e21322a8094a00241c04738
destinybarbery <destiny.barbery@gmail.com>
2022-03-05 20:55:51 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
 
 function App() {
   return (
-    <h1>test of project</h1>
+    <div className= "container">
+      <div>
+        questions here
+      </div>
+      <div>
+        
+      </div>
+    </div>
   );
 }
 
