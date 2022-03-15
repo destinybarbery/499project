21602ce5da4ef81afa2eb42707e6512fc152ab45
Elvis Cirikovic <ccirikovic.elvis@gmail.com>
2022-03-15 00:35:48 -0400

    the score thing doesnt work im just pushing this for now but imma fix it in the morning

--- public/script.js
+++ public/script.js
@@ -4,6 +4,10 @@ const questionContainerElement = document.getElementById("question-container")
 const questionElement = document.getElementById('question')
 let shuffledQuestions, currentQuestionIndex
 const answerButtonsElement = document.getElementById('answer-buttons')
+const scoreText = document.getElementById('score');
+let TEMPPOINTS = 5;
+
+let score = 0;
 
 
 startButton.addEventListener("click", startGame)
@@ -38,6 +42,7 @@ function showQuestion(question)
         if(answer.correct)
         {
             button.dataset.correct = answer.correct
+
         }
         button.addEventListener("click",selectAnswer)
         answerButtonsElement.appendChild(button)
@@ -59,9 +64,10 @@ function selectAnswer(e)
     const selectedButton = e.target;
     const isCorrect = selectedButton.dataset.correct;
     setStatusClass(document.body, isCorrect)
-    Array.from(answerButtonsElement.children).forEach(button =>{
+    Array.from(answerButtonsElement.children).forEach(button =>
+        {
         setStatusClass(button, button.dataset.correct)
-    })
+        })
     if(shuffledQuestions.length > currentQuestionIndex+1)
     {
         nextButton.classList.remove("hide")
@@ -71,20 +77,29 @@ function selectAnswer(e)
         startButton.innerText = "Restart"
         startButton.classList.remove("hide")
     }
+    scoreCounter(-5);
     
 }
 
 function setStatusClass(element, isCorrect)
-{
+{   if(isCorrect)
+    {
+        scoreCounter(TEMPPOINTS);
+    }
+    {
+        element.classList.add("correct")
+
+    }
     clearStatusClass(element)
     if(isCorrect)
     {
         element.classList.add("correct")
-        
+
     }
     else
     {
         element.classList.add("wrong")
+        
     }
 }
 
@@ -329,3 +344,9 @@ const questions =
         },
     
 ]
+
+scoreCounter = num => 
+{
+    score +=num;
+    scoreText.innerText = score;
+}
