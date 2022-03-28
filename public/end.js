7acbc7c0758a2a6e5b035a50daeee233cf5bb85a
Elvis Cirikovic <ccirikovic.elvis@gmail.com>
2022-03-28 00:15:24 -0400

    just pushing, i have to pick up at scores.js

--- public/end.js
+++ public/end.js
@@ -2,9 +2,32 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("FinalScore");
 const mostRecentScore = localStorage.getItem("mostRecentscore");
+const saveButton = document.getElementById("save-button");
 
-//const highScores = JSON.parse(localStorage.getItem(highScores)) || [];
-const MAX_HIGH_SCORES = 5;
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+
+const firebaseConfig = 
+{
+   apiKey: "AIzaSyCsi7k_Qf_Rubmx5fmQnXmP1eGosKqYbEA",
+   authDomain: "quizapp-d375c.firebaseapp.com",
+   databaseURL: "https://quizapp-d375c-default-rtdb.firebaseio.com",
+    projectId: "quizapp-d375c",
+    storageBucket: "quizapp-d375c.appspot.com",
+    messagingSenderId: "102216189713",
+    appId: "1:102216189713:web:3ee38fb0f5c3c5b9b7eeb0"
+}
+
+//init firebase
+initializeApp(firebaseConfig)
+
+//init database services
+const db = getFirestore()
+
+//reference firestore database
+const collectionRef = collection(db, 'scores')
 
 
 finalScore.innerText = mostRecentScore;
@@ -12,21 +35,32 @@ username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.ariaValueMax;
 });
 
+saveButton.addEventListener("click",saveHighScore)
 
-
-saveHighScore = e => {
+function saveHighScore()  // !!!!!!! i continue from here when i get home !!!!!!!!!!
+{
     console.log("clicked the save button!");
     e.preventDefault();
 
+    addDoc(colRef,{
+        mode: "regular",
+        name: username.value,
+        score: finalScore.value
+    })
+    .then(() =>{
+        console.log("score successfully saved")
+        //window.location("scores.html")
+    })
 
-const score = {
-    score: Math.floor(Math.random() * 100),
-    name: username.value
-};
-highScores.push(score)
-highScores.sort((a,b) => b.score - a.score)
-highScores.splice(5);
-localStorage.setItem("highScore", JSON.stringify(highScores));
-window.location.assign("/");
+
+//const score = {
+    //score: Math.floor(Math.random() * 100),
+   // name: username.value
+//};
+//highScores.push(score)
+//highScores.sort((a,b) => b.score - a.score)
+//highScores.splice(5);
+//localStorage.setItem("highScore", JSON.stringify(highScores));
+//window.location.assign("/");
 };
 
--- public/scores.js
+++ public/scores.js
@@ -1,9 +1,37 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
-    .map(score => {
-        return '<li class="high-score">${score.name} - ${score.score}</li>';
-    
-    })
-    .join("")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+
+const firebaseConfig = 
+{
+   apiKey: "AIzaSyCsi7k_Qf_Rubmx5fmQnXmP1eGosKqYbEA",
+   authDomain: "quizapp-d375c.firebaseapp.com",
+   databaseURL: "https://quizapp-d375c-default-rtdb.firebaseio.com",
+    projectId: "quizapp-d375c",
+    storageBucket: "quizapp-d375c.appspot.com",
+    messagingSenderId: "102216189713",
+    appId: "1:102216189713:web:3ee38fb0f5c3c5b9b7eeb0"
+}
+
+//init firebase
+initializeApp(firebaseConfig)
+
+//init database services
+const db = getFirestore()
+
+//reference firestore database
+const collectionRef = collection(db, 'scores')
+
+getDocs(collectionRef)
+    .then((snapshot)=> {
+        let scores = []
+        snapshot.docs.forEach((doc) =>
+        {
+            scores.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(scores)
+    }) 
+    .catch(err =>
+        {
+            console.log(err.message)
+        })
